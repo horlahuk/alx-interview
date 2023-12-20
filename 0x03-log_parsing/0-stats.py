a1af89defcfc84766554ffad69a055a5a75ef192
horlahuk <olaitan.u.k@gmail.com>
2023-12-20 12:10:53 +0100

    task 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,41 +4,36 @@
 import sys
 
 
-def printsts(dic, size):
-    """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+def print_stats(status_codes, file_size):
+    """Prints the stats"""
+    print("File size: {}".format(file_size))
+    for key in sorted(status_codes.keys()):
+        if status_codes[key] != 0:
+            print("{}: {}".format(key, status_codes[key]))
 
-
-sts = {"200": 0, "301": 0, "400": 0, "401": 0,
-       "403": 0,"404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+file_size = 0
+counter = 0
 
 try:
     for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
-
-        stlist = line.split()
-        count += 1
-
+        if counter != 0 and counter % 10 == 0:
+            print_stats(status_codes, file_size)
+        
+        counter += 1
+        data = line.split()
         try:
-            size += int(stlist[-1])
+            file_size += int(data[-1])
         except:
             pass
-
         try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
+            if data[-2] in status_codes:
+                status_codes[data[-2]] += 1
         except:
             pass
-    printsts(sts, size)
-
+    print_stats(status_codes, file_size)
 
 except KeyboardInterrupt:
-    printsts(sts, size)
+    print_stats(status_codes, file_size)
     raise
